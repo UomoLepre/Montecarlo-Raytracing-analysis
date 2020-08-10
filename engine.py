769ac6f2b3fdb719156f3f8b474410e8b15acc51
UomoLepre <caparvigiorgio@yahoo.it>
2020-08-10 16:13:42 +0200

    First commit

---
+++ engine.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+npart = 1000
+nx =400
+ny =400
+
+
+s = (nx,ny) #setting matrix dimension
+
+hyst = np.zeros(s) #float64
+a = np.zeros(s, dtype=int)#int64
+
+logical = True
+
+
